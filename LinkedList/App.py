0768af5ab3dee3a5d3c33818a576681399cdc6d9
Rene Nederhand <rene@cogfor.com>
2015-09-14 10:08:37 +0200

    Initial LinkedList implementation

---
+++ LinkedList/App.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+App.py:
+"""
+from LinkedList import LinkedList
+print("Start")
+LinkedList = LinkedList()
+
+LinkedList.insertEnd(12)
+LinkedList.insertEnd(122)
+LinkedList.insertEnd(3)
+LinkedList.insertStart(31)
+
+LinkedList.traverseList()
+
+LinkedList.remove(12)
+
+LinkedList.traverseList()
+print("End")
+
+
+
+
