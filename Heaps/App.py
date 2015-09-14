b3f3d48a451dc4d86819ef4220e8bfad4b601a3d
Rene Nederhand <rene@cogfor.com>
2015-09-14 16:33:13 +0200

    Heap and heap sort implementation

---
+++ Heaps/App.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+App:
+"""
+
+from Heap import Heap
+
+heap = Heap()
+
+heap.insert(12)
+heap.insert(-3)
+heap.insert(23)
+heap.insert(4)
+
+heap.heapsort()
+
+
---
+++ Heaps/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+__init__.py:
+"""
+
+__version__ = '0.0.1'
+
+__copyright__ = 'Copyright 2015, Cogfor <info@cogfor.com>. All rights reserved.'
+__license__ = 'All rights reserved.'
+
+
+
